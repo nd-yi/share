b1ead309036dedf1cc407429bd7cc194d49ff097
SongYi <songyi@nd.com.cn>
2022-02-11 18:13:58 +0800

    add adopt feature

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+import { tns } from './src/tiny-slider.js';
+console.log('========================= ~ tns', tns)
+
+var slider = tns({
+  container: '.user-profile-pets',
+  items: 3,
+  slideBy: 'page',
+  speed: 400,
+  mouseDrag: true,
+  autoplay: false
+});
+
+
 const handleClick = () => {
   console.log('========dgsd')
   const targetUrl = 'https://apps.apple.com/us/app/pet/id1560912975';
